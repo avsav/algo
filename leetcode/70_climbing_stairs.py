8c52607bf8bd101d636064fd570fa7fc54494397
Anastasiia <avsav999@mail.ru>
2024-12-26 01:11:46 +0300

    Bad optimal dp solution

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -17,16 +17,23 @@ class Solution(object):
             if n == 1: return 1
             return f(n - 1) + f(n - 2)
         
-        # my memo solution
-        def mf(n):
-            arr = []
-            arr.append(1)
-            arr.append(1)
+        # my dp solution
+        def dpf(n):
+            dp = [0] * (n + 1)
+            dp[0] = dp[1] = 1
             for i in range(2, n + 1):
-                arr.append(arr[i - 1] + arr[i - 2])
-            return arr[n]
+                dp[i] = dp[i - 1] + dp[i - 2]
+            return dp[n]
         
-        return mf(n)
+        # optimal dp solution
+        def odpf(n):
+            prev = next = 1
+            for i in range(2, n + 1):
+                prev = next
+                next = prev + next
+            return next        
+        
+        return odpf(n)
     
 
      
