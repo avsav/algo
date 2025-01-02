2895e8a34f0d04e6bb3e960a2b31ef504df16514
Anastasiia <avsav999@mail.ru>
2025-01-02 15:20:25 +0300

    .

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 class Graph:
     # Constructor
         # Adjacancy list
-        def __init__(self, num_of_nodes, directed=False):
+        def __init__(self, num_of_nodes, directed=True):
             self._num_of_nodes = num_of_nodes
             self._nodes = range(self._num_of_nodes)
 
@@ -58,9 +58,7 @@ graph.add_edge(0, 1, 5)
 graph.add_edge(0, 2, 3)
 graph.add_edge(1, 3, 1)
 graph.add_edge(1, 4, 15)
-graph.add_edge(4, 2, 7)
-graph.add_edge(4, 3, 11)
-#graph.remove_edge(0, 2, 3)
-#graph.bfs(0, 4)
+graph.add_edge(2, 5, 7)
+graph.bfs(0, 4)
 
-graph.print_adj_list()
+#graph.print_adj_list()
