233d58c2e0ebade9460569b09365a2dfc7193bb9
Anastasiia <avsav999@mail.ru>
2025-01-06 01:06:02 +0300

    Add creatte_cycled_list

--- leetcode/141_linked_list_cycle.py
+++ leetcode/141_linked_list_cycle.py
@@ -29,22 +29,25 @@ class LinkedList:
         return curr
 
     def return_node_at_pos(self, pos):
+        if self.h is None:
+            return 
         curr = self.h
         n = 0
         while curr.next and n != pos:
             n += 1
             curr = curr.next
-        print(curr.val)
+        return curr
 
     def create_cycled_list(self, head, pos):
         n = len(head)
-        #if pos == 0:
 
         for i in range(n):
             self.add_node_to_end(head[i])
         
-        #node = ListNode(head[n - 1])
-        #node.next = 
+        node_at_pos = self.return_node_at_pos(pos)
+        node_at_end = self.return_node_at_end()
+
+        node_at_end.next = node_at_pos
 
 
     def print_ll(self):
@@ -60,6 +63,6 @@ ll = LinkedList()
 ll.create_cycled_list(head, 0)
 #ll.hasCycle(head)
 #ll.return_node_at_end()
-ll.return_node_at_pos(2)
+ll.return_node_at_pos(0)
 #ll.print_ll()
 
