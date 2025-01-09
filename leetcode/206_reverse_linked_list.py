ee80380af55613f84e2ebd195c229ccbc7675a70
Anastasiia <avsav999@mail.ru>
2025-01-09 21:57:46 +0300

    Add description

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -3,6 +3,21 @@
 #from mlinked_list import *
 
 """
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = node
+        head = prev
+        return head
 """
 
