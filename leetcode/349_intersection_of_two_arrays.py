5ad5bc1da17d17b59bdac5bbf9d4eddf585d0f6c
Anastasiia <avsav999@mail.ru>
2025-01-29 18:16:53 +0300

    Accepted

---
+++ leetcode/349_intersection_of_two_arrays.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+def intersection(nums1, nums2):
+    nums1.sort()
+    nums2.sort()
+    
+    n1 = len(nums1)
+    n2 = len(nums2)
+    len_ht1 = nums1[n1 - 1] + 1
+    len_ht2 = nums2[n2 - 1] + 1
+    ht1 = [0] * (len_ht1)
+    ht2 = [0] * (len_ht2)
+
+    for num in nums1:
+        ht1[num] += 1
+    
+    for num in nums2:
+        ht2[num] += 1
+
+    res = []
+    for i in range(min(len_ht1, len_ht2)):
+        if ht1[i] > 0 and ht2[i] > 0:
+            res.append(i)
+    
+    return res
+
+        
+nums1 = [4,9,5]
+nums2 = [9,4,9,8,4]
+print(intersection(nums1, nums2))
