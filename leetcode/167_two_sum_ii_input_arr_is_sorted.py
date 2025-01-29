d9d3e4f877da591a8876e51e9dfb7bc36c81626f
Anastasiia <avsav999@mail.ru>
2025-01-29 14:53:44 +0300

    Accepted!

--- leetcode/167_two_sum_ii_input_arr_is_sorted.py
+++ leetcode/167_two_sum_ii_input_arr_is_sorted.py
@@ -1,19 +1,19 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, numbers, target):
         left = 0
         for right in range(1, len(numbers)):
             diff = target - numbers[left]
-            num = self.binarySearch(numbers[right:], diff)
+            num = self.binarySearch(numbers, diff, right)
             if num == -1:
                 left += 1
             else:
-                return [left + 1, right + 1]
+                return [left + 1, num + 1]
             
     
-        def binarySearch(self, nums, target):
-        low = 0
+    def binarySearch(self, nums, target, left):
+        low = left
         high = len(nums) - 1
 
         while low <= high:
@@ -21,9 +21,12 @@ class Solution:
             if nums[mi] > target:
                 high = mi - 1
             elif nums[mi] < target:
-                low = nums[mi] + 1
+                low = mi + 1
             else:
                 return mi
         return -1
     
 
+obj = Solution()
+nums = [2, 3, 4]
+print(obj.twoSum(nums, 6))
