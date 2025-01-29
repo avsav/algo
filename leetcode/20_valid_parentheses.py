3ba7fb8f351baad29facbda5a6797eff36850668
Anastasiia <avsav999@mail.ru>
2025-01-29 15:17:49 +0300

    Accepted

--- leetcode/20_valid_parentheses.py
+++ leetcode/20_valid_parentheses.py
@@ -7,13 +7,18 @@ class Solution:
         for symb in s:
             if symb in pairs.keys():
                 stack.append(symb)
+            elif not stack and symb not in pairs.keys():
+                return False
             else:
                 open_bracket = stack.pop()
                 if pairs[open_bracket] == symb:
-                    stack.pop()
+                    continue
+                else:
+                    return False
         return len(stack) == 0 
     
 
 obj = Solution()
-s = input("Введите строку: ")
+#s = input("Введите строку: ")
+s = ")"
 print(obj.isValid(s))
