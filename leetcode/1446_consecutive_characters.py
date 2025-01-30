fc4feb6e44355cea73026b802da57ba7b0f52cf0
Anastasiia <avsav999@mail.ru>
2025-01-31 00:48:21 +0300

    Accepted

--- leetcode/1446_consecutive_characters.py
+++ leetcode/1446_consecutive_characters.py
@@ -1,4 +1,22 @@
 # https://leetcode.com/problems/consecutive-characters/
 
+from collections import defaultdict
+
 def maxPower(s):
-    pass
+    n = len(s)
+    cnt = maks = 1
+    for i in range(1, n):
+        if s[i] == s[i - 1]:
+            cnt += 1
+            maks = max(maks, cnt)
+        else:
+            cnt = 1
+    if maks == 0:
+        return 1
+    return maks
+    
+
+s1 = "leetcode"
+s2 = "abbcccddddeeeeedcba"
+s3 = "tourist"
+print(maxPower(s1))
