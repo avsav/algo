a419b591fbc20615430c1ce089a0f011afccd5fd
Anastasiia <avsav999@mail.ru>
2025-02-06 20:33:37 +0300

    .

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -15,8 +15,8 @@ def merge(nums1, m, nums2, n):
     return nums1
 
 
-nums1 = [2,0]
-m = 1
-nums2 = [1]
-n = 1
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
 print(merge(nums1, m, nums2, n))
