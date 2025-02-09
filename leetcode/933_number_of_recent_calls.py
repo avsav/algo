c46526b0c5a3738fb23951287ca3ead337d3c20c
Anastasiia <avsav999@mail.ru>
2025-02-09 23:23:45 +0300

    .

---
+++ leetcode/933_number_of_recent_calls.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/number-of-recent-calls/description/
+
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        li = self.requests
+        n = len(li)
+        for l in li:
+            if l < t - 3000:
+                n -= 1
+            else:
+                return n
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+obj = RecentCounter()
+param_1 = obj.ping(1)
+param_2 = obj.ping(100)
+param_3 = obj.ping(3001)
+param_4 = obj.ping(3002)
+print(param_1)
+print(param_2)
+print(param_3)
+print(param_4)
