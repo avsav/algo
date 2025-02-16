dc994c6ea9ebae6bf22acdceae73c88c209e48e3
Anastasiia <avsav999@mail.ru>
2025-02-17 00:25:12 +0300

    Accepted --- Smart Solution!

--- leetcode/215_kth_largest_element_in_an_array.py
+++ leetcode/215_kth_largest_element_in_an_array.py
@@ -1,12 +1,27 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 
-from random import randrange
-
+#from random import randrange
+from collections import defaultdict
 
 def findKthLargest(nums, k):
-    qsort(nums, 0, len(nums) - 1)
-    return nums[len(nums) - k]
+    #qsort(nums, 0, len(nums) - 1)
+    max = -10**10
+    for num in nums:
+        if num > max:
+            max = num
+    ht = defaultdict(int)
+    #ht = [0] * len(nums)
+    for num in nums:
+        d = max - num
+        ht[d] += 1
+    d = cnt = 0
+    while cnt < k:
+        cnt += ht[d]
+        d += 1
+    return max - d + 1
 
+
+"""
 def rand_partition(nums, a, b):
     n = randrange(a, b + 1)
     nums[n], nums[b] = nums[b], nums[n]
@@ -24,9 +39,10 @@ def qsort(nums, a, b):
         pivot = rand_partition(nums, a, b)
         qsort(nums, a, pivot - 1)
         qsort(nums, pivot + 1, b)
+"""
 
-
-nums = [2,8,7,1,3,5,6,4]
-qsort(nums, 0, len(nums) - 1)
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4
+#qsort(nums, 0, len(nums) - 1)
 print(nums)
-print(findKthLargest(nums, 2))
+print(findKthLargest(nums, k))
