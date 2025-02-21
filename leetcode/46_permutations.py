daf893eecff37b6290b78652ee28e259e046afe1
Anastasiia <avsav999@mail.ru>
2025-02-21 11:08:09 +0300

    .

--- leetcode/46_permutations.py
+++ leetcode/46_permutations.py
@@ -1,5 +1,31 @@
 # https://leetcode.com/problems/permutations/
 
 def permute(nums):
-    pass
+    res = [nums]
+    res.append(next_permutation(nums))
+    if nums == nums[::-1]:
+        return
+    return permute(nums)
+
+def next_permutation(nums):
+    n = len(nums)
+    k = n - 2
+    while nums[k] > nums[k + 1]:
+        k -= 1
+    t = k + 1
+    while t < n - 1 and nums[t + 1] > nums[k]:
+        t += 1
+    nums[k], nums[t] = nums[t], nums[k]
+    if k >= 0:
+        for i in range(k + 1, n//2):
+            nums[i] = nums[n - k - i]
+    return nums
+
+nums1 = [1, 3, 5, 4, 2]
+nums2 = [1, 2, 3]
+nums3 = [3, 2, 1]
+print(next_permutation(nums3))
+#print(permute(nums2))
+    
+
 
---
+++ leetcode/50_pow(x,n).py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/powx-n/description/
+
+def myPow(x, n):
+    res =  1
+    m = abs(n)
+    while m:
+        if m&1:
+            res *= x
+        x *= x
+        m >>= 1
+    if n < 0:
+        return 1/res
+    return res
+
+x = 5
+n = -3
+print(myPow(x, n))
