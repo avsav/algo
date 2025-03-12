3d28a280496bc0c23890b3ec22ccf6fbb02f832d
Anastasiia <avsav999@mail.ru>
2025-03-12 16:06:05 +0300

    Accepted

--- leetcode/443_string_compression.py
+++ leetcode/443_string_compression.py
@@ -3,23 +3,29 @@
 def compress(chars):
     n = len(chars)
     right = 0
-    spi = []
-    res = 0
+    k = 0
     while right < n:
         left = right
         cnt = 0
-        res += 1
-        spi.append(chars[left])
+        chars[k] = chars[right] 
         while right < n and chars[left] == chars[right]:
             right += 1
             cnt += 1
-    if cnt > 10:
-        lcnt = [_ for _ in str(cnt)]
-        spi.extend(lcnt)
-        res += len(str(cnt))
-    return spi
+        scnt = str(cnt)
+
+        left = k
+        if cnt > 1:
+            for i in range(left + 1, left + len(scnt) + 1):
+                chars[i] = scnt[i - left - 1]
+            k = left + len(scnt) + 1
+        else:
+            k = left + 1       
+
+    return len(chars[:k])
 
 
-
-chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-print(compress(chars))
+chars1 = ["a","a","a"]
+chars2 = ["a","a","a","a","b","b","b","b","b","b","b","b","b","b","b","b"]
+chars3 = ["a"]
+chars4 = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+print(compress(chars4))
