5548ca7999e13c2ea4c4582e2b4a208b708a07c3
Anastasiia <avsav999@mail.ru>
2025-03-28 14:16:27 +0300

    Not accepted 29/31 testcases passed

---
+++ leetcode/1089_duplicate_zeros.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/duplicate-zeros/description/
+
+
+def duplicateZeros(arr):
+    n = len(arr)
+    last = 0
+    cnt = 0
+    for i in range(n):
+        if arr[i] == 0:
+            cnt += 2
+        else:
+            cnt += 1
+        if cnt == n:
+            last = i
+            break
+
+    i = n - 1
+    while i >= 0:
+        arr[i] = arr[last]
+        if arr[last] == 0:
+            i -= 1
+            arr[i] = 0
+        i -= 1
+        last -= 1
+
+    return arr
+
+    
+
+
+arr1 = [1,0,2,3,0,4,5,0]
+arr2 = [1,2,3]
+arr3 = [8,4,5,0,0,0,0,7]
+print(duplicateZeros(arr3))
