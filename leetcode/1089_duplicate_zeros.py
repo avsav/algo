e613a8b42a2037220f32238fad35f60b188fb6e7
Anastasiia <avsav999@mail.ru>
2025-03-28 15:19:39 +0300

    Accepted

--- leetcode/1089_duplicate_zeros.py
+++ leetcode/1089_duplicate_zeros.py
@@ -5,6 +5,7 @@ def duplicateZeros(arr):
     n = len(arr)
     last = 0
     cnt = 0
+    flag = False
     for i in range(n):
         if arr[i] == 0:
             cnt += 2
@@ -13,15 +14,20 @@ def duplicateZeros(arr):
         if cnt == n:
             last = i
             break
+        if cnt - n == 1:
+            flag = True
+            last = i
+            break
 
     i = n - 1
     while i >= 0:
         arr[i] = arr[last]
-        if arr[last] == 0:
+        if arr[last] == 0 and not flag:
             i -= 1
             arr[i] = 0
         i -= 1
         last -= 1
+        flag = False
 
     return arr
 
@@ -31,4 +37,4 @@ def duplicateZeros(arr):
 arr1 = [1,0,2,3,0,4,5,0]
 arr2 = [1,2,3]
 arr3 = [8,4,5,0,0,0,0,7]
-print(duplicateZeros(arr3))
+print(duplicateZeros(arr2))
