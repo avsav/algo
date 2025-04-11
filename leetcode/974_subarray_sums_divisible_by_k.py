f189ba2fd355b80d7974cb8d1b6bb36c15eef1e8
Anastasiia <avsav999@mail.ru>
2025-04-11 11:30:21 +0300

    if k == 2 it's okay!

--- leetcode/974_subarray_sums_divisible_by_k.py
+++ leetcode/974_subarray_sums_divisible_by_k.py
@@ -3,22 +3,22 @@
 def subarraysDivByK(nums, k):
     n = len(nums)
     prefix = res = 0
-    divByK = notDivByK = 0
+    divByK = 1
+    notDivByK = 0
     for num in nums:
         prefix += num
         if prefix % k:
+            res += notDivByK
             notDivByK += 1
-            res += divByK + 1
         else:
+            res += divByK
             divByK += 1
-            res += notDivByK 
-
-
+            
     return res
 
 
 k1 = 2
-nums1 = [1,3]
+nums1 = [1,3,5]
 k2 = 5
 nums2 = [4,5,0,-2,-3,1]
 k3 = 9
