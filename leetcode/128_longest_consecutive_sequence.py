2974d506151e8b7bed8ba6dbdcd451359d67a1b8
Anastasiia <avsav999@mail.ru>
2025-04-16 17:50:12 +0300

    Accepted

--- leetcode/128_longest_consecutive_sequence.py
+++ leetcode/128_longest_consecutive_sequence.py
@@ -3,15 +3,18 @@
 from collections import defaultdict
 
 def longestConsecutive(nums):
-    n = len(nums)
     nums = set(nums)
-    hm = defaultdict(list)
+    maxLen = 0
     for num in nums:
-        hm[num] = num + 1
-    return hm
+        if num - 1 not in nums:
+            nyam = num + 1
+            while nyam in nums:
+                nyam += 1
+            maxLen = max(maxLen, nyam - num)
+    return maxLen
 
 
 nums1 = [100,4,200,1,3,2]
 nums2 = [0,3,7,2,5,8,4,6,0,1]
 nums3 = [1,0,1,2]
-print(longestConsecutive(nums3))
+print(longestConsecutive(nums1))
