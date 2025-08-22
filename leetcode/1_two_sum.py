e96ee3c99484cc53a26db6a018e55e5373f18dd4
Anastasiia <avsav999@mail.ru>
2025-08-22 22:07:48 +0300

    Add twoSum2 O(n)

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/two-sum/description/
 
+from collections import defaultdict
+
 def twoSum(nums, target):
     n = len(nums)
     for i in range(n):
@@ -8,6 +10,30 @@ def twoSum(nums, target):
                 return [i,j]
                 
 
-nums = [int(item) for item in input().split()]
-target = int(input())
-print(twoSum(nums, target))
+def twoSum2(nums, target):
+    n = len(nums)
+    d = defaultdict(int)
+
+    for i in range(n):
+        d[nums[i]] = i
+
+    for i in range(n):
+        compl = target - nums[i]
+        if compl in d and i != d[compl]:
+            return [i, d[compl]]
+    
+    return []
+            
+
+
+
+
+nums1 = [1,5,7,2,4]
+target1 = 3
+nums2 = [2,7,11,15]
+target2 = 9
+nums3 = [3,2,4]
+target3 = 6
+nums4 = [3,3]
+target4 = 6
+print(twoSum2(nums3, target3))
