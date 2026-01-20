9344fdc4478d69799a50228d9a45118b399ecf70
Anastasiia <avsav999@mail.ru>
2026-01-20 20:11:47 +0300

    Pass my tests

--- leetcode/849_max_dist_to_closet_person.py
+++ leetcode/849_max_dist_to_closet_person.py
@@ -3,16 +3,21 @@ def maxDistToClosest(seats):
     i = 0
     max_dist = 1
     for j in range(1, n):
-        if seats[i] == 1:
+        if seats[i] == 0 and seats[j] == 1:
             max_dist = max(max_dist, j - i)
             i = j
         if seats[i] == 1 and seats[j] == 1:
             max_dist = max(max_dist, (j - i)//2)
             i = j
-        if seats[n - 1] == 0:
+        if j == n - 1 and seats[j] == 0:
             max_dist = max(max_dist, n - 1 - i)
     return max_dist
 
 
-seats = [1,0,0,0,1,0,1]
-print(maxDistToClosest(seats))
+seats1 = [1,0,0,0,1,0,1]
+seats2 = [1,0,0,0]
+seats3 = [0,1]
+seats4 = [0,1,1]
+seats5 = [0,0,0,0,0,1]
+seats6 = [1,0,0,0,1,0,1,0,0,0,0,0,0]
+print(maxDistToClosest(seats6))
