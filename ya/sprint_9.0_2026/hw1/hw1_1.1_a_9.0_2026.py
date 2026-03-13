847e2d440cedac06dcb051298c70935feebe6a54
Anastasiia <avsav999@mail.ru>
2026-03-13 19:36:59 +0300

    Add hw1

---
+++ ya/sprint_9.0_2026/hw1/hw1_1.1_a_9.0_2026.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def f():
+    return 0
+
+
+def main():
+    """
+    Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+    n = int(input())
+    print(n)
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
