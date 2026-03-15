f76ae28042ce23bc26d89530567b003278d554a8
Anastasiia <avsav999@mail.ru>
2026-03-15 22:45:29 +0300

    draft 1.1a and 1.1b

--- ya/sprint_9.0_2026/hw1/hw1_1.1_a_9.0_2026.py
+++ ya/sprint_9.0_2026/hw1/hw1_1.1_a_9.0_2026.py
@@ -1,17 +1,35 @@
 import sys
 
 
-def f():
-    pass
+def f(p, v, q, m):
+    a = p - v
+    b = p + v
+    c = q - m
+    d = q + m
+    n = 0
+    if b < c:
+        n = b - a + d - c + 2
+    if d < a:
+        n = d - c + b - a + 2
+    if b == c or b > c and d > a:
+        n = d - a + 1
+    if d == a or d > a and b > c:
+        n = b - c + 1
+    if a < c and d < b:
+        n = b - a + 1
+    if c < a and b < d:
+        n = d - c + 1
+    return n
 
 
 def main():
-    """
-    Пример ввода и вывода числа n, где -10^9 < n < 10^9:
-    n = int(input())
-    print(n)
-    """
-    pass
+    #p, v = map(int, input().split())
+    #q, m = map(int, input().split())
+    p1, v1 = 0, 7
+    q1, m1 = 12, 5
+    p2, v2 = 1, 3
+    q2, m2 = 6, 1
+    print(f(p2, v2, q2, m2))
 
 
 if __name__ == '__main__':
---
+++ ya/sprint_9.0_2026/hw1/hw1_1.1_b_9.0_2026.py
@@ -0,0 +1,74 @@
+import sys
+import math
+
+
+def set_of_floors(m, k2, p2, n2):
+    a = max(math.ceil(k2 / (n2 + m*(p2 - 1))), math.ceil(k2 / (m*p2))) 
+    if p2 != 1 and n2 == 1:
+        b = k2 / (m*(p2 - 1))
+    if p2 == 1 and n2 != 1:
+        b = k2 / (n2 - 1)
+    if p2 != 1 and n2 != 1:
+        b = min(k2 / (m*(p2 - 1)), k2 / (n2 - 1))
+    k_set = set()
+    k = a
+    while k >= a and k < b:
+        k_set.add(k)
+        k += 1
+    return k_set
+
+
+def f(k1, m, k2, p2, n2):
+    if p2 == 1 and n2 == 1:
+        if m == 1:
+            print("0 1")
+            return
+        if k1 <= k2:
+            print("1 1")
+            return
+        if k1 <= k2*m:
+            print("1 0")
+            return
+        print("0 0")
+        return
+    
+    k_set = set_of_floors(m, k2, p2, n2)
+    if n2 > m or not k_set:
+        print("-1 -1")
+        return
+
+    n1_set = set()
+    for k in k_set:
+        if k1 <= k*m:
+            p1 = 1
+            n1 = math.ceil(k1 / k)
+        if k1 > k*m:
+            p1 = math.ceil(k1 / (k*m))
+            n1 = math.ceil((k1 - k*m*(p1 - 1)) / k)
+        n1_set.add(n1)
+
+    if len(k_set) > 1:
+        p1 = 0
+        if len(n1_set) > 1:
+            n1 = 0
+
+    print(f"{p1} {n1}")
+    return 
+
+
+def main():
+    #k1, m, k2, p2, n2 = map(int, input().split())
+    k11, m1, k21, p21, n21 = 89, 20, 41, 1, 11  #output 2 3
+    k12, m2, k22, p22, n22 = 11, 1, 1, 1, 1     #output 0 1
+    k13, m3, k23, p23, n23 = 3, 2, 2, 2, 1      #output -1 -1
+    k14, m4, k24, p24, n24 = 118, 9, 115, 4, 2  #output 4 3
+    k15, m5, k25, p25, n25 = 11, 2, 4, 1, 2     #output 0 2
+    k16, m6, k26, p26, n26 = 5, 20, 2, 1, 1     #output 1 0
+    k17, m7, k27, p27, n27 = 25, 3, 1, 1, 1     #output 0 0
+    k18, m8, k28, p28, n28 = 1, 3, 1, 3, 1      #output -1 -1
+    k19, m9, k29, p29, n29 = 1, 3, 1, 1, 3      #output -1 -1
+    f(k19, m9, k29, p29, n29)
+
+
+if __name__ == '__main__':
+    main()
