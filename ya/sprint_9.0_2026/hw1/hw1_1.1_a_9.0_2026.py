fd43ea4fe8b04455f69e1eee601b2f3ce4a9de53
Anastasiia <avsav999@mail.ru>
2026-03-16 11:57:24 +0300

    OK

--- ya/sprint_9.0_2026/hw1/hw1_1.1_a_9.0_2026.py
+++ ya/sprint_9.0_2026/hw1/hw1_1.1_a_9.0_2026.py
@@ -11,9 +11,13 @@ def f(p, v, q, m):
         n = b - a + d - c + 2
     if d < a:
         n = d - c + b - a + 2
-    if b == c or b > c and d > a:
+    if b == c:
         n = d - a + 1
-    if d == a or d > a and b > c:
+    if d == a:
+        n = b - c + 1
+    if b > c and d > b and c > a:
+        n = d - a + 1
+    if d > a and b > d and a > c:
         n = b - c + 1
     if a < c and d < b:
         n = b - a + 1
@@ -25,11 +29,11 @@ def f(p, v, q, m):
 def main():
     #p, v = map(int, input().split())
     #q, m = map(int, input().split())
-    p1, v1 = 0, 7
-    q1, m1 = 12, 5
-    p2, v2 = 1, 3
-    q2, m2 = 6, 1
-    print(f(p2, v2, q2, m2))
+    p1, v1, q1, m1 = 0, 7, 12, 5                    #output 25
+    p2, v2, q2, m2 = 1, 3, 6, 1
+    p3, v3, q3, m3 = -10**8, 10**8, 10**8, 10**8
+    p4, v4, q4, m4 = -1, 12, 8, 17                  #output 39
+    print(f(p4, v4, q4, m4))
 
 
 if __name__ == '__main__':
