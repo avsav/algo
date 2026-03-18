d254f25912e1e95b2d1ee08cf9a725d470d71b9f
Anastasiia <avsav999@mail.ru>
2026-03-18 09:32:36 +0300

    Update README.md

---
+++ ya/sprint_9.0_2026/comp1/comp1_a_9.0_2026.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def f(n, m, a):
+    pass
+
+
+def main():
+    """
+    Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+    n = int(input())
+    print(n)
+    """
+    n, m = map(int, input().split())
+    m = int(input())
+    print(n, m)
