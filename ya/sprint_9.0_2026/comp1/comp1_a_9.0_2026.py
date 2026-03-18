1b32fe1f27ee42ea421e4e7ac20d02ec21d727eb
Anastasiia <avsav999@mail.ru>
2026-03-18 16:23:48 +0300

    OK

--- ya/sprint_9.0_2026/comp1/comp1_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp1/comp1_a_9.0_2026.py
@@ -2,13 +2,20 @@ import sys
 
 
 def f(n, m, a):
-    pass
+    res = 0
+    for i in range(n):
+        for j in range(m):
+            if j + 1 < m and a[i][j] == "." and a[i][j + 1] == ".":
+                res += 1
+            if i + 1 < n and a[i][j] == "." and a[i + 1][j] == ".":
+                res += 1
+    return res
 
 
 def main():
     n, m = map(int, input().split())
     a = [input() for _ in range(n)]
-    print(a)
+    print(f(n, m, a))
 
 
 if __name__ == '__main__':
