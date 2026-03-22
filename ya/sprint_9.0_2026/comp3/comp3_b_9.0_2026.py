f329791c4baaf847a0190713c8e31a03ec23d09e
Anastasiia <avsav999@mail.ru>
2026-03-22 15:47:10 +0300

    b OK

--- ya/sprint_9.0_2026/comp3/comp3_b_9.0_2026.py
+++ ya/sprint_9.0_2026/comp3/comp3_b_9.0_2026.py
@@ -4,14 +4,15 @@ import sys
 def f(n):
     ans = n - 1
     for var in {n, 2*n - 1, 2*n, 2*n + 1}:
-        for r in range(1, n + 1):
+       r = 1
+       while r*r <= var:
             if var % r == 0:
                 if var == n:
                     m = var // r
                 else:
                     m = (var // r + 1) // 2
                 ans = min(ans, abs(r - m))
-
+            r += 1
     return ans
 
 
@@ -21,7 +22,7 @@ def main():
     n2 = 2 #output 1
     n3 = 1 #output 0
     n4 = 50 #output 3
-    print(f(n4))
+    print(f(n1))
 
 
 if __name__ == '__main__':
