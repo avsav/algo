d8bc848bc89856651f08d222b619bdcbc521b65f
Anastasiia <avsav999@mail.ru>
2026-03-28 15:59:02 +0300

    Good version?

--- ya/sprint_9.0_2026/comp6/comp6_b_9.0_2026.py
+++ ya/sprint_9.0_2026/comp6/comp6_b_9.0_2026.py
@@ -2,33 +2,21 @@ import sys
 
 
 def f(n, a):
-    sa = ''
-    for i in range(1, n + 1):
-        sa += str(i) 
-
-    ans = n
-    k = 0
+    ans = 0
     for _ in range(n):
-        for i in range(n):
-            if sa[i] == a[i]:
-                k += 1
-                break
-            if i == n - 1 and sa[i] != a[i]:
-                ans = k
-                return ans
-            
-        a = int(a)
-        a = str(a % 10) + str(a // 10)
-
-        ans = min(ans, k)
+        if all(a[i] != i + 1 for i in range(n)):
+            return ans
+        else:
+            ans += 1
+        last = a.pop()
+        a.insert(0, last)
 
     return -1
 
 
 def main():
     n = int(input())
-    a = input()
-    a = ''.join(a.split())
+    a = [int(i) for i in input().split()]
     print(f(n, a))
 
 
