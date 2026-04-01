7897616149dd38296c75f67bfc057b301f4c4752
Anastasiia <avsav999@mail.ru>
2026-04-01 12:50:33 +0300

    TL

--- ya/sprint_9.0_2026/comp7/comp7_b_9.0_2026.py
+++ ya/sprint_9.0_2026/comp7/comp7_b_9.0_2026.py
@@ -3,13 +3,21 @@ import sys
 
 def f(n, types):
     ans = 0
-
+    n = len(types)
+    ans = max_len = 0
+    d = {}
+    for i in range(n):
+        d[types[i]] = d.get(types[i], 0) + 1
+        cnts = [*d.values()]
+        if len(cnts) >= 2:
+            max_len = cnts[-1] + cnts[-2]
+        ans = max(ans, max_len)
     return ans
 
 
 def main():   
     n = int(input())
-    types = list(map(int, input()))
+    types = list(map(int, input().split()))
     print(f(n, types))
 
 
