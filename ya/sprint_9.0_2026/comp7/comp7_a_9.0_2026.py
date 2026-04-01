1761ee66e155ee350c97b7c6d8722e40eba5a769
Anastasiia <avsav999@mail.ru>
2026-04-01 08:07:09 +0300

    .

--- ya/sprint_9.0_2026/comp7/comp7_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp7/comp7_a_9.0_2026.py
@@ -5,7 +5,7 @@ def f(n, string):
     nums = [1 if s == 'a' else -1 for s in string]
     n = len(nums)
     prefix = 0
-    d = {0: 1}
+    d = {0 : 1}
     ans = 0
     for i in range(n):
         prefix += nums[i]
