d1a3569142e9c8fc9cb777aa42adbe9efeeff989
Anastasiia <avsav999@mail.ru>
2026-04-05 17:46:14 +0300

    WA 10

--- ya/sprint_9.0_2026/comp9/comp9_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp9/comp9_a_9.0_2026.py
@@ -2,19 +2,28 @@ import sys
 
 
 def f(string):
-    ans = ""
-        
-    return ans
+    n = len(string)
+    scores = [ord(s) - ord("A") for s in string] 
+    bad_score = max(scores)
+    avg_score = sum(scores) / n
+    avg_score = int(avg_score) if avg_score - int(avg_score) < .5 else int(avg_score) - 1
+    avg_score = bad_score - 1 if bad_score - avg_score > 1 else avg_score
+    return chr(avg_score + ord("A")) 
 
 
 def main():
-    #string = input()
+    """
+    string = input()
+    print(f(string))
+    """
+    #"""
     string1 = "ABACABA"
     print(string1, f(string1) == "B")
     string2 = "AZAA"
     print(string2, f(string2) == "Y")    
     string3 = "ABABAB"
     print(string3, f(string3) == "A")
+    #"""
 
 
 if __name__ == '__main__':
