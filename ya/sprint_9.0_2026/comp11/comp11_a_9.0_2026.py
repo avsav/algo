f3d7c1eead2eb3151dda98a1dd4b970fd921cb4d
Anastasiia <avsav999@mail.ru>
2026-04-10 22:47:02 +0300

    WA 10

--- ya/sprint_9.0_2026/comp11/comp11_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp11/comp11_a_9.0_2026.py
@@ -18,7 +18,7 @@ def f(string):
     k = 0
     min_sum = curr_sum = 0
     for i in range(n):
-        curr_sum += 0 if string[i] in "([{" else -1
+        curr_sum += 1 if string[i] in "([{" else -1
         if min_sum > curr_sum:
             min_sum = curr_sum
             k = i
@@ -32,11 +32,18 @@ def f(string):
 def main():
     #string = input()
     string1 = "}()[]{"
+    print(string1, f(string1) == "YES")
     string2 = "}([)]{"
+    print(string2, f(string2) == "NO")
     string3 = "()]["
+    print(string3, f(string3) == "YES")
     string4 = "][]][{}]{()}[]{({})()}(){}{}[](){}{}{()}[][{}[]{}[{}]()][]{}()[][]()[]{}[]()()()([([])][])[]{}{}(()){}[]()()[(())][]()[]{{}}{}[][[]()()]{}{}(){[()]}[]{[]}[]()[]()[][](){[]}{}[]()()[][[]]({{}})[{}(){}["
-    print(f(string4))
-
+    print(string4, f(string4) == "YES")
+    string5 = "][]][{}(){}[" 
+    print(string5, f(string5) == "YES")
+    string6 = "()" 
+    print(string6, f(string6) == "YES")
+    
 
 if __name__ == '__main__':
     main()
