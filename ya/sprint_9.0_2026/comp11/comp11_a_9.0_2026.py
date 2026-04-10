66604995741aec5252ed76b65ca3cbd7d31359b4
Anastasiia <avsav999@mail.ru>
2026-04-10 21:54:17 +0300

    WA 14

--- ya/sprint_9.0_2026/comp11/comp11_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp11/comp11_a_9.0_2026.py
@@ -1,16 +1,41 @@
 import sys
 
 
-def f(string):
-    ans = ""
-
+def valid_parenthesis(string):
+    parenthesis = ["()", "[]", "{}"]
+    stack = []
+    for s in string:
+        if stack and stack[-1] + s in parenthesis:
+            stack.pop()
+        else: 
+            stack.append(s)
     
-    return ans
+    return not stack
+
+
+def f(string):
+    n = len(string)
+    k = 0
+    min_sum = curr_sum = 0
+    for i in range(n):
+        curr_sum += 0 if string[i] in "([{" else -1
+        if min_sum > curr_sum:
+            min_sum = curr_sum
+            k = i
+    
+    if valid_parenthesis(string[k + 1:] + string[:k + 1]):
+        return "YES"
+
+    return "NO"
 
 
 def main():
-    string = input()
-    print(f(string))
+    #string = input()
+    string1 = "}()[]{"
+    string2 = "}([)]{"
+    string3 = "()]["
+    string4 = "][]][{}]{()}[]{({})()}(){}{}[](){}{}{()}[][{}[]{}[{}]()][]{}()[][]()[]{}[]()()()([([])][])[]{}{}(()){}[]()()[(())][]()[]{{}}{}[][[]()()]{}{}(){[()]}[]{[]}[]()[]()[][](){[]}{}[]()()[][[]]({{}})[{}(){}["
+    print(f(string4))
 
 
 if __name__ == '__main__':
