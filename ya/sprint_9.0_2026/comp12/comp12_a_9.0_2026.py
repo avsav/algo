0b851eeea28ad7491ad26cf0a75849bba0034e02
Anastasiia <avsav999@mail.ru>
2026-04-12 15:04:11 +0300

    OK

--- ya/sprint_9.0_2026/comp12/comp12_a_9.0_2026.py
+++ ya/sprint_9.0_2026/comp12/comp12_a_9.0_2026.py
@@ -3,26 +3,38 @@ import sys
 
 def f(balls):
     n = int(balls[0])
-    balls += "10"
-    d = {}
+    del_ball = None
     stack = []
-    for i in range(1, n + 2):
-        if stack and balls[i] != stack[-1] and d[stack[-1]] >= 3:
-            last = stack[-1]
-            while d[last] > 0:
-                d[last] -= 1
-                stack.pop()
+    for i in range(1, n + 1):
+        if len(stack) >= 2 and stack[-2] == stack[-1] and stack[-1] == balls[i]:
+            stack.pop()
+            stack.pop()
+            del_ball = balls[i]
 
-        if i != n + 1:
+        if balls[i] != del_ball:
+            del_ball = None
             stack.append(balls[i])
-            d[balls[i]] = d.get(balls[i], 0) + 1
 
     return n - len(stack)
 
 
 def main():
-    balls = list(input().split())
-    print(f(balls))
+    #balls = list(input().split())
+    #print(f(balls))
+    balls1 = [5,1,3,3,3,2]
+    print(balls1, f(balls1) == 3)
+    balls2 = [10,3,3,2,1,1,1,2,2,3,3]
+    print(balls2, f(balls2) == 10)
+    balls3 = [3,0,0,0]
+    print(balls3, f(balls3) == 3)
+    balls4 = [10,9,9,2,2,9,9,9,9,2,2]
+    print(balls4, f(balls4) == 8)
+    balls5 = [31,1,0,0,2,2,3,3,4,4,5,5,6,6,7,7,8,8,8,8,7,6,6,5,5,4,3,3,0,0,1,1]
+    print(balls5, f(balls5) == 22)
+    balls6 = [100,4,4,3,4,3,3,4,3,4,4,5,4,3,3,4,4,3,3,4,3,3,4,4,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,3,3,4,4,5,4,3,3]
+    print(balls6, f(balls6) == 67)
+    balls7 = [15,4,4,3,4,3,3,4,3,4,4,5,4,4,4,4,5,3,3,4,4,5,4,3,3]
+    print(balls7, f(balls7) == 4)
 
 
 if __name__ == '__main__':
