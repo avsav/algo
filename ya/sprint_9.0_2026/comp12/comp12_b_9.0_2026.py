e187cabd7cbb4036fa5c53b034e5fa7a0a99327f
Anastasiia <avsav999@mail.ru>
2026-04-13 00:52:48 +0300

    OK 1st attempt  13.04.2026 00:48

--- ya/sprint_9.0_2026/comp12/comp12_b_9.0_2026.py
+++ ya/sprint_9.0_2026/comp12/comp12_b_9.0_2026.py
@@ -1,50 +1,28 @@
 import sys
 
 
-def ship_type(field, r, c):
-    type = 0
-    hor = False
-    k = 1
-    while field[r][c + k] != ".":
-        hor = True
-        if field[r - 1][c + k] != "." or field[r + 1][c + k] != ".":
-            return -1       
-        k += 1
-    vert = False
-    m = 1
-    while field[r + m][c] != ".":
-        vert = True
-        if field[r + m][c - 1] != "." or field[r + m][c + 1] != ".":
-            return -1       
-        m += 1
-
-    if hor:
-        type = k
+def del_head(field):
+    new_field = [list(f) for f in field]
+    cnt = 0
+    for r in range(1, 11):
+        for c in range(1, 11):
+            if field[r][c] == "#":
+                if field[r - 1][c - 1] == "." and field[r][c - 1] == "." and field[r - 1][c] == ".":
+                    new_field[r][c] = "."
+                    cnt += 1
     
-    if vert:
-        type = m
+    new_field = ["".join(nf) for nf in new_field]
 
-    if not hor and not vert:
-        type = 1
-
-    if type > 4:
-        type = -1
-
-    return type
+    return new_field, cnt
 
 
 def f(field):
-    corr_cnt = {1 : 4, 2 : 3, 3 : 2, 4 : 1}
-    cnt = {1 : 0, 2 : 0, 3 : 0, 4 : 0}
-    for r in range(1, 11):
-        for c in range(1, 11):
-            if field[r][c] == "#" and field[r][c - 1] == "." and field[r - 1][c] == ".":
-                type = ship_type(field, r, c)
-                if type == -1:
-                    return "NO"
-                cnt[type] += 1
-    
-    if cnt == corr_cnt:
+    field10, cnt10 = del_head(field)
+    field6, cnt6 = del_head(field10)
+    field3, cnt3 = del_head(field6)
+    field1, cnt1 = del_head(field3)
+
+    if cnt10 == 10 and cnt6 == 6 and cnt3 == 3 and cnt1 == 1:
         return "YES"
     
     return "NO"
@@ -55,7 +33,6 @@ def main():
     field.insert(0, "." * 12)
     field.append("." * 12)
     print(f(field))
-    #print(ship_type(field, 7, 8))
 
 
 if __name__ == '__main__':
